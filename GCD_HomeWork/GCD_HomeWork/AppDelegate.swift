95ca2f6328e3b656095bbed95928595756a69b2a
tiennt-0361 <nguyen.thanh.tien-b@sun-asterisk.com>
2023-08-29 08:45:50 +0700

    Init project

---
+++ GCD_HomeWork/GCD_HomeWork/AppDelegate.swift
@@ -0,0 +1,7 @@
+
+import UIKit
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
---
+++ GCD_HomeWork/GCD_HomeWork/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GCD_HomeWork
+//
+//  Created by Thanh Duong on 29/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
